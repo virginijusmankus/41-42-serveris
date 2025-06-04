42ef4cac9a29841549b81cc6d9e24a092be4f9b4
Virgis Mankus <virgismankus@gmail.com>
2025-06-04 18:22:03 +0300

    lyg baigiau

--- js/create.js
+++ js/create.js
@@ -1,24 +1,3 @@
-// const form = document.querySelector('form')
-// console.log(form)
-
-// const createPost = async (e) => {
-//     e.preventDefault()
-//     const doc = {
-//         title: form.title.value,
-//         body:form.body.value,
-//         views: 0
-//     }
-//     await fetch('http://localhost:3000/posts',{
-//         method:'POST',
-//         body:JSON.stringify(doc),
-//         headers:{'Content-Type':'application/json'}
-//     })
-//     window.location.replace('index.html')
-// }
-// form.addEventListener('submit', createPost)
-
-
-
 document.querySelector('form').addEventListener('submit', async (e) => {
   e.preventDefault();
 
@@ -27,14 +6,11 @@ document.querySelector('form').addEventListener('submit', async (e) => {
   const body = form.body.value;
   const file = form.image.files[0];
 
-  const reader = new FileReader();
-  reader.onload = async function () {
-    const base64Image = reader.result;
-
+  const submitPost = async (imageData = null) => {
     const post = {
       title,
       body,
-      image: base64Image,
+      image: imageData,
       id: Date.now().toString()
     };
 
@@ -45,37 +21,20 @@ document.querySelector('form').addEventListener('submit', async (e) => {
     });
 
     if (res.ok) {
-      // Alert pirmas – rodomas trumpai, tada keliaujam
-      alert("Straipsnis sukurtas!");
-      // Priverstinai pereina į index.html
-      window.location.href = "index.html";
+      window.location.href = window.location.origin + window.location.pathname.replace("create.html", "index.html");
+
     } else {
       alert("Nepavyko išsaugoti straipsnio");
     }
   };
 
   if (file) {
-  reader.readAsDataURL(file);
-} else {
-  // Jei paveikslėlis nepasirinktas – tiesiog sukurk be jo
-  const post = {
-    title,
-    body,
-    image: null,
-    id: Date.now().toString()
-  };
-
-  const res = await fetch('http://localhost:3000/posts', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(post)
-  });
-
-  if (res.ok) {
-    alert("Straipsnis sukurtas!");
-    window.location.href = "index.html";
+    const reader = new FileReader();
+    reader.onload = function () {
+      submitPost(reader.result);
+    };
+    reader.readAsDataURL(file);
   } else {
-    alert("Nepavyko išsaugoti straipsnio");
+    submitPost(null);
   }
-}
 });
