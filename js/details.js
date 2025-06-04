dd31cd7d9e8941646798cf0d58727dfaafc71c44
Virgis Mankus <virgismankus@gmail.com>
2025-06-04 18:29:08 +0300

    Baigta

--- js/details.js
+++ js/details.js
@@ -1,23 +1,57 @@
-const id = new URLSearchParams(window.location.search).get('id')
-const container = document.querySelector('.details')
+// const id = new URLSearchParams(window.location.search).get('id')
+// const container = document.querySelector('.details')
 
-window.addEventListener('DOMContentLoaded',()=>renderDetails())
+// window.addEventListener('DOMContentLoaded',()=>renderDetails())
 
-const renderDetails = async ()=>{
-    let uri = 'http://localhost:3000/posts/'+id;
+// const renderDetails = async ()=>{
+//     let uri = 'http://localhost:3000/posts/'+id;
 
-    const res = await fetch(uri)
-    const post = await res.json()
-    const template = `
+//     const res = await fetch(uri)
+//     const post = await res.json()
+//     const template = `
+//     <h1>${post.title}</h1>
+//     <p>${post.body}</p>
+//     `
+//     container.innerHTML=template;
+// }
+// const deleteBtn = document.querySelector('.delete')
+// deleteBtn.addEventListener('click',async(e)=>{
+//     const res = await fetch('http://localhost:3000/posts/'+id,{
+//       method:'DELETE'  
+//     })
+// window.location.replace('./index.html')
+// })
+
+
+
+
+
+
+const id = new URLSearchParams(window.location.search).get('id');
+const container = document.querySelector('.details');
+
+window.addEventListener('DOMContentLoaded', () => renderDetails());
+
+const renderDetails = async () => {
+  let uri = 'http://localhost:3000/posts/' + id;
+
+  const res = await fetch(uri);
+  const post = await res.json();
+
+  const template = `
+    ${post.image ? `<img src="${post.image}" alt="Straipsnio nuotrauka" style="max-width:100%; border-radius:10px; margin-bottom:20px;">` : ''}
     <h1>${post.title}</h1>
     <p>${post.body}</p>
-    `
-    container.innerHTML=template;
-}
-const deleteBtn = document.querySelector('.delete')
-deleteBtn.addEventListener('click',async(e)=>{
-    const res = await fetch('http://localhost:3000/posts/'+id,{
-      method:'DELETE'  
-    })
-window.location.replace('./index.html')
-})
+  `;
+
+  container.innerHTML = template;
+};
+
+// Trinimo mygtukas
+const deleteBtn = document.querySelector('.delete');
+deleteBtn.addEventListener('click', async (e) => {
+  const res = await fetch('http://localhost:3000/posts/' + id, {
+    method: 'DELETE'
+  });
+  window.location.replace('index.html');
+});
